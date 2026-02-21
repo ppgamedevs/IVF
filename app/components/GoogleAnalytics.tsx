4433b0b72ec08feee3b1e092aeefc9f91c986d1e
ppgamedevs <ppgamedevs@gmail.com>
2026-02-21 17:41:12 +0200

    Refactor GoogleAnalytics component to standardize script loading strategy by changing 'lazyOnLoad' to 'lazyOnload' for consistency across all script tags.

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -9,7 +9,7 @@ export default function GoogleAnalytics() {
       {/* Google Tag Manager - loads first if configured */}
       {GTM_ID && (
         <>
-          <Script id="gtm-init" strategy="lazyOnLoad">
+          <Script id="gtm-init" strategy="lazyOnload">
             {`
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -26,9 +26,9 @@ export default function GoogleAnalytics() {
         <>
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-            strategy="lazyOnLoad"
+            strategy="lazyOnload"
           />
-          <Script id="gtag-init" strategy="lazyOnLoad">
+          <Script id="gtag-init" strategy="lazyOnload">
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
